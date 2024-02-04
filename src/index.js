3d663b3201573141382b824f5dcabf916353e061
Santiago Delebecq <santiidele21@gmail.com>
2024-02-04 00:07:35 -0300

    add gitignore

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const router = require("./routes/routes");
-const server = express();
+const app = express();
 const path = require("path");
 //mongoose
 const connectDB = require("./model/mongodb/connection");
 const PORT = process.env.PORT ?? 3000;
 // Configurar el motor de plantillas EJS
-server.set("view engine", "ejs");
-server.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // Servir archivos estáticos desde la carpeta 'public'
-server.use(express.static(path.join(__dirname, "public")));
-server.use(router);
-server.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(router);
+app.use(express.json());
 // Llama a la función para conectar a la base de datos
 connectDB()
   .then(() => {
     // Inicia el servidor Express una vez que la conexión a la base de datos se haya establecido correctamente
-    server.listen(PORT, () => {
+    app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
